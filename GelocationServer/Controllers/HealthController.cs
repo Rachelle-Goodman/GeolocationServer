25a871e855329be6f956a396a493fae52c55fede
tehilaja <tehilaj97@gmail.com>
2020-08-03 13:37:31 +0300

    hello api

---
+++ GelocationServer/Controllers/HealthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GelocationServer.Controllers
+{
+    public class HealthController : Controller
+    {
+        [HttpGet("hello")]
+        public ActionResult Hello()
+        {
+            return Ok();
+        }
+    }
+}
