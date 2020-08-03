2c3f996c33619aec0e9202cb596c0a55844b460e
tehilaja <tehilaj97@gmail.com>
2020-08-03 13:54:57 +0300

    Revert "fix configuarions"
    This reverts commit 6548318a2bf80f24e3a567c1a13500a370ae1d8b.

--- GelocationServer/Startup.cs
+++ GelocationServer/Startup.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace GelocationServer
 {
@@ -16,13 +15,7 @@ namespace GelocationServer
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc(mvcOptions =>
-            {
-                mvcOptions.EnableEndpointRouting = false;
-            });
-        }
+        public void ConfigureServices(IServiceCollection services) { }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
