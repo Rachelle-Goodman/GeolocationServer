85725ee53823b8e594719cbe49ce8ee09f222f9f
tehilaja <tehilaj97@gmail.com>
2020-08-03 22:51:44 +0300

    change health check
    make health check find all dynamodb entities on its own, and check each one automatically

--- Geolocation.Utilities.Aws.DynamoDB/DynamoDbHealthTester.cs
+++ Geolocation.Utilities.Aws.DynamoDB/DynamoDbHealthTester.cs
@@ -4,6 +4,8 @@ using Geolocation.Constants;
 using Geolocation.Utilities.Aws.DynamoDB.Entities;
 using Geolocation.Utilities.Encryption;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Geolocation.Utilities.Aws.DynamoDB
@@ -14,16 +16,41 @@ namespace Geolocation.Utilities.Aws.DynamoDB
         {
             var awsAccessKey = DesEncryptor.DecryptData(Environment.GetEnvironmentVariable(EnvironmentVariablesNames.ENCRYPTED_AWS_ACCESS_KEY));
             var awsSecretAccessKey = DesEncryptor.DecryptData(Environment.GetEnvironmentVariable(EnvironmentVariablesNames.ENCRYPTED_AWS_SECRET_ACCESS_KEY));
-
             var client = new AmazonDynamoDBClient(awsAccessKey, awsSecretAccessKey, RegionEndpoint.USEast1);
-
-            await Task.WhenAll(
-                CheckTableHealth<PlaceDdbDto>(client),
-                CheckTableHealth<DistanceDdbDto>(client),
-                CheckTableHealth<SearchDdbDto>(client));
+            List<Type> dynamoDbEntities = GetDynamoDbEntities();
+            await CheckTablesHealth(client, dynamoDbEntities);
         }
 
-        private static async Task CheckTableHealth<T>(AmazonDynamoDBClient client) where T : DynamoDbEntityBase
+        private static async Task CheckTablesHealth(AmazonDynamoDBClient client, List<Type> dynamoDbEntities)
+        {
+            List<Task> checkTablesHealthTasks = new List<Task>();
+
+            foreach (var type in dynamoDbEntities)
+            {
+                var method = typeof(DynamoDbHealthTester).GetMethod(nameof(DynamoDbHealthTester.CheckTableHealth));
+                object checkTableHealthTask = method.MakeGenericMethod(type).Invoke(obj: null, parameters: new object[] { client });
+                checkTablesHealthTasks.Add((Task)checkTableHealthTask);
+            }
+
+            await Task.WhenAll(checkTablesHealthTasks);
+        }
+
+        private static List<Type> GetDynamoDbEntities()
+        {
+            var dynamoDbEntities = new List<Type>();
+
+            foreach (var domain_assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                var assembly_types = domain_assembly.GetTypes()
+                  .Where(type => type.IsSubclassOf(typeof(DynamoDbEntityBase)) && !type.IsAbstract);
+
+                dynamoDbEntities.AddRange(assembly_types);
+            }
+
+            return dynamoDbEntities;
+        }
+
+        public static async Task CheckTableHealth<T>(AmazonDynamoDBClient client) where T : DynamoDbEntityBase
             => await client.DescribeTableAsync(DynamoDbUtil.GetTableName<T>());
     }
 }
